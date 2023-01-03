adc26615963e1fa924a246902f55f9e978c8aaf3
jakubmosakowski <jmosakowski@spoton.com>
2023-01-03 18:22:40 +0100

    Cleanup

--- app/src/main/java/com/example/nav_experiments/ui/doublenested/DoubleNestedFragment.kt
+++ app/src/main/java/com/example/nav_experiments/ui/doublenested/DoubleNestedFragment.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.IdRes
-import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.fragment.findNavController
-import com.example.nav_experiments.databinding.FragmentNestedBinding
+import com.example.nav_experiments.databinding.FragmentDoubleNestedBinding
 
 class DoubleNestedFragment : Fragment() {
 
-    private var _binding: FragmentNestedBinding? = null
+    private var _binding: FragmentDoubleNestedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +23,7 @@ class DoubleNestedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentNestedBinding.inflate(inflater, container, false)
+        _binding = FragmentDoubleNestedBinding.inflate(inflater, container, false)
         binding.textHome.setOnClickListener {
             navigateBackToStartDestination()
         }
@@ -38,14 +36,6 @@ class DoubleNestedFragment : Fragment() {
     }
 
     fun navigateBackToStartDestination() {
-        println()
-        println()
-        println()
-        navController.backQueue.forEach { println(it.destination) }
-        println()
-        println()
-        println()
-
         val startDestination = navController.graph.findStartDestination()
         println("To be found: $startDestination")
         println("Back stack:")
@@ -57,30 +47,11 @@ class DoubleNestedFragment : Fragment() {
 
     private fun navigateUpTo(
         destinationId: Int,
-        fallback: () -> Unit = { },
     ) {
-
-        navController.navigateUp()
-        println("Back stack:")
-        navController.backQueue.forEach { println(it.destination) }
-
         while (navController.currentBackStackEntry?.destination?.id != destinationId) {
             navController.navigateUp()
             println("Back stack:")
             navController.backQueue.forEach { println(it.destination) }
-        }
-    }
-
-    fun navigate(@IdRes resId: Int, @Nullable args: Bundle? = null) {
-        navController.navigate(resId, args)
-    }
-
-    private fun isInBackStack(@IdRes destinationId: Int): Boolean {
-        return try {
-            navController.getBackStackEntry(destinationId)
-            true
-        } catch (e: IllegalArgumentException) {
-            false
         }
     }
 }
